8d05784232d7aca17e5efc46b548e36af5d1363f
Attila Vigh <legguss@gmail.com>
2023-02-08 18:17:30 +0000

    fix backlashes in the path

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 
 import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app\\app.component';
-import { AppRoutingModule } from './app\\app-routing.module';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
