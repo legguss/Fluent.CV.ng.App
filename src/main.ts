7f6239c1863ae3b0f5223fca9c00070b702599ea
Attila Vigh <legguss@gmail.com>
2023-02-08 17:58:39 +0000

    remove NgModules

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app\\app.component';
 import { AppRoutingModule } from './app\\app-routing.module';
